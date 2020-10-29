0fc9d0571b180697e9aed1c708b6e85f52531d6f
Kash <amahlw@gmail.com>
2020-10-29 17:44:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-
     return 'Are you there, world? It\'s me, Ducky!'
 
 
- @app.route('/')
-  def homepage():
-
-    return 'Are you there, world? It\'s me, Ducky!'
+@app.route('/animal/<users_animal>')
+def favorite_animal(users_animal):
+    # users_animal = 'lion'
+    return f'Wow, {users_animal} is my favorite animal, too!'
 
 
 if __name__ == '__main__':
