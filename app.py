1d2e99b7d244c0b99b6037f53dd445b72050ffe9
Kash <amahlw@gmail.com>
2020-10-29 02:40:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,11 @@ def homepage():
     return 'Are you there, world? It\'s me, Ducky!'
 
 
+ @app.route('/')
+  def homepage():
+
+    return 'Are you there, world? It\'s me, Ducky!'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
