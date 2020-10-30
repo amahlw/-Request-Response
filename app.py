3fad7a32a40bf05555f1537ed2ddf7a96c6b5c87
Kash <amahlw@gmail.com>
2020-10-30 11:02:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,22 @@ def favorite_animal(users_animal):
     return f'Wow, {users_animal} is my favorite animal, too!'
 
 
+@app.route('/dessert/<users_dessert>')
+def favorite_desserts(users_dessert):
+    return f'Wow how did you know i like {users_dessert}'
+
+
+@app.route('/madlibs/<adjective>/<noun>')
+def mad_libs(adjective, noun):
+    return f'{noun} is {adjective}!'
+
+
+@app.route('/multiply/<number1>/<number2>')
+def multiple(num1, num2):
+    int(num1) * int(num2):
+
+    return f'{int((num1)}*{num2} is'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
